5217b1c4b691c58bfcb2d57345784f1fc1052bfa
TehSl0th <57567551+TehSl0th@users.noreply.github.com>
2021-06-17 09:20:29 +0200

    Initial commit

---
+++ SimpleChess/Bishop.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace SimpleChess
+{
+    class Bishop : Piece
+    {
+        // Vi arver 'string Symbol' fra 'Piece'. Jeg prøvde først 'public Bishop(string symbol) : base(symbol)' men innså seinere at vi ikke skal ta in noe.
+        // Nå arver vi 'symbol' men vi setter symbol til å være "LPR" som var symbolet til 'Bishop' før jeg endret på den.
+        public Bishop() : base("LPR")
+        {
+        }
+
+        // En override som setter Move til hva 'Bishop' gjorde fra før av.
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            var diffCol = fromPosition[0] - toPosition[0];
+            var diffRow = fromPosition[1] - toPosition[1];
+            Console.WriteLine(diffCol);
+            return Math.Abs(diffRow) == Math.Abs(diffCol);
+        }
+    }
+}
---
+++ SimpleChess/Rook.cs
@@ -0,0 +1,18 @@
+﻿namespace SimpleChess
+{
+    class Rook : Piece
+    {
+        // Vi arver 'string Symbol' fra 'Piece'. Jeg prøvde først 'public Rook(string symbol) : base(symbol)' men innså seinere at vi ikke skal ta in noe.
+        // Nå arver vi 'symbol' men vi setter symbol til å være "TRN" som var symbolet til 'Rook' før jeg endret på den.
+        public Rook() : base("TRN")
+        {
+
+        }
+
+        // En override som setter Move til hva 'Rook' gjorde fra før av.
+        public override bool Move(string fromPosition, string toPosition)
+        {
+            return fromPosition[0] == toPosition[0] || fromPosition[1] == toPosition[1];
+        }
+    }
+}
